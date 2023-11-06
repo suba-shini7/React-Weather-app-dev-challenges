ee8b195d82f8e72555e485f2bde8cd2b20302354
Subashini <125429575+suba-shini7@users.noreply.github.com>
2023-11-06 21:11:03 +0530

    Delete src directory

--- src/Components/Todaycard.js
+++
@@ -1,9 +0,0 @@
-export const TodayCard = (props) => {
-  return (
-    <div className="Todaycard">
-      <p>{props.header}</p>
-      <div>{props.icon}</div>
-      <p>{props.values}</p>
-    </div>
-  );
-};
